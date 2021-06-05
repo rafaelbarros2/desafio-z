35b0ceef06e5ae19a7191b901a86511aebdccc2e
Rafael Barros <rafael.eng.mecanic@gmail.com>
2021-06-05 13:47:31 -0300

    primeiro commit

---
+++ src/test/java/com/desafioz/DesafioZApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafioz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioZApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
