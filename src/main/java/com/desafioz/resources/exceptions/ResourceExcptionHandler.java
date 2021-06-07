ee659e17eb7063c6c978d694bca0faeb265032fc
Rafael Barros <rafael.eng.mecanic@gmail.com>
2021-06-06 22:23:22 -0300

    tratamento da exceção dele

--- src/main/java/com/desafioz/resources/exceptions/ResourceExcptionHandler.java
+++ src/main/java/com/desafioz/resources/exceptions/ResourceExcptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.desafioz.services.exceptions.DatabaseException;
 import com.desafioz.services.exceptions.ResourceNotFoundExceptions;
 
 
@@ -20,6 +21,14 @@ public class ResourceExcptionHandler {
 	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundExceptions e, HttpServletRequest request){
 		String error = "Resource not found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
+		String error = "Database Error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
